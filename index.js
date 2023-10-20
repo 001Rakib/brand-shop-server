8782548a6c8c943e3e87b442aa22ed4757dbab14
001Rakib <rakibrakibrakib354@gmail.com>
2023-10-20 11:00:29 +0600

    added another get method to a get a specefic product to update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,6 +34,12 @@ async function run() {
     app.get("/brandProducts", async (req, res) => {
       const cursor = brandsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/brandProducts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await brandsCollection.findOne(query);
       res.send(result);
     });
     app.post("/brandProducts", async (req, res) => {
